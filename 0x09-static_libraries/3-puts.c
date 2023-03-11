65bc3afcc6c03ae720dd2eb65f46a409a7891036
Emmanuel Njenga <wathiomonjenga@gmail.com>
2023-03-10 17:28:21 -0800

    Static Libraries

---
+++ 0x09-static_libraries/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+void _puts(char *str);
+
+int main(void)
+{
+    _puts("\"Programming is like building a multilingual puzzle");
+    return (0);
+}
+
+void _puts(char *str)
+{
+    puts(str);
+}
