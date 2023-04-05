5dda9c2d81cb08aec6612a10bea59c8f880b984b
Emmanuel Njenga <wathiomonjenga@gmail.com>
2023-04-05 12:29:23 -0700

    0-print_listint.c

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,28 @@
+#include <stdio>
+#include "list.h"
+
+
+/**
+  *print_listint - function that prints all the elements of a listint_t list.
+  *@h : a  pointer to head of linked list
+  *
+  *author - Emmanuel Njenga
+  *
+  *Return : the number of nodes in the list
+  *
+  */
+
+
+size_t print_listint (const listint_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
---
+++ 0x13-more_singly_linked_lists/README.md
@@ -0,0 +1,15 @@
+0x13. C - More singly linked lists
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ *
+ */
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *next;
+} listint_t;
