9d2067c015422bc6e75ca64118789c435d24e210
Emmanuel Njenga <wathiomonjenga@gmail.com>
2023-04-05 16:54:19 -0700

    delete_nodeint

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -16,35 +16,34 @@
  * Return: pointer to the indexed node
  */
 
-listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
 	unsigned int i;
-	listint_t *new_node, *current_node;
+	listint_t *current_node, *previous_node;
 
-	new_node = malloc(sizeof(listint_t));
+	if (*head == NULL)
+		return (-1);
 
-	if (new_node == NULL)
-		return (NULL);
-
-	new_node->n = n;
-
-	if (idx == 0)
+	if (index == 0)
 	{
-		new_node->next = *head;
-		*head = new_node;
-		return (new_node);
+		current_node = *head;
+		*head = (*head)->next;
+		free(current_node);
+		return (1);
 	}
+
+	previous_node = NULL;
 	current_node = *head;
 
-	for (i = 0; i < idx - 1 && current_node != NULL; i++)
-		current_node = current_node->next;
-
-	if (current_node == NULL)
+	for (i = 0; i < index && current_node != NULL; i++)
 	{
-		free(new_node);
-		return (NULL);
+		previous_node = current_node;
+		current_node = current_node->next;
 	}
-	new_node->next = current_node->next;
-	current_node->next = new_node;
-	return (new_node);
+	if (current_node == NULL)
+		return (-1);
+	previous_node->next = current_node->next;
+	free(current_node);
+	return (1);
 }
