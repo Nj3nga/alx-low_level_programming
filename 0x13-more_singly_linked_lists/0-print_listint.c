44037601ef1036e096e87a80ae749671afd408c2
Emmanuel Njenga <wathiomonjenga@gmail.com>
2023-04-05 13:45:04 -0700

    0-print_listint.c

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -5,12 +5,13 @@
   * print_listint - Prints all the elements of a listint_t list.
   * @h: a pointer to the head of the linked list
   *
-  * Return: the number of nodes in the list
   *
   * Description:function prints all elements of linked list of type listint_t.
   * Takes a pointer to head of the linked list as input, and iterates through
   * the list, printing value of each node and updating pointer to the next node
   * The function returns the number of nodes in the list.
+  *
+  *Return: number of elements in th list
   *
   */
 
